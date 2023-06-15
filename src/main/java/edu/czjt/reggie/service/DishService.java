bd044aea90d31ef95554c3bda1c1e1d48b097400
wxs <124442376+yvxiangqiongsi@users.noreply.github.com>
2023-06-15 22:24:36 +0800

    Update DishService.java

--- src/main/java/edu/czjt/reggie/service/DishService.java
+++ src/main/java/edu/czjt/reggie/service/DishService.java
@@ -1,6 +1,14 @@
 package edu.czjt.reggie.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.czjt.reggie.dto.DishDto;
+import edu.czjt.reggie.entity.Dish;
 
-public interface DishService  {
+/**
+ * Created by wangxiaoshan
+ */
+public interface DishService extends IService<Dish> {
+    public DishDto getByIdWithFlavor(Long id);
 
+    public void saveWithFlavor(DishDto dishDto);
 }
