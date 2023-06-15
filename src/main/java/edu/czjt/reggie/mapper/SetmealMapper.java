a13a7c4c391b51115f49e2502f2d1e1e76287217
次元菌 <135974633+hxx2799@users.noreply.github.com>
2023-06-15 22:09:56 +0800

    Update SetmealMapper.java

--- src/main/java/edu/czjt/reggie/mapper/SetmealMapper.java
+++ src/main/java/edu/czjt/reggie/mapper/SetmealMapper.java
@@ -1,5 +1,8 @@
 package edu.czjt.reggie.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.Setmeal;
+import org.apache.ibatis.annotations.Mapper;
 
-public interface SetmealMapper  {
+public interface SetmealMapper extends BaseMapper<Setmeal> {
 }
