f52ecf36b4a9d42d998e0037cf68015aecfba578
wxs <124442376+yvxiangqiongsi@users.noreply.github.com>
2023-06-15 21:45:07 +0800

    Update DishMapper.java

--- src/main/java/edu/czjt/reggie/mapper/DishMapper.java
+++ src/main/java/edu/czjt/reggie/mapper/DishMapper.java
@@ -1,7 +1,12 @@
 package edu.czjt.reggie.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.Dish;
+import org.apache.ibatis.annotations.Mapper;
 
-
-
-public interface DishMapper {
+/**
+ * Created by wangxiaoshan
+ */
+@Mapper
+public interface DishMapper extends BaseMapper<Dish> {
 }
