9e29963ecab6f77555dd0683d8290235c1023b0b
dengxinghua <129150926+dengxinghuaHH@users.noreply.github.com>
2023-06-16 08:27:44 +0800

    Update AddressBookService.java
    更新

--- src/main/java/edu/czjt/reggie/service/AddressBookService.java
+++ src/main/java/edu/czjt/reggie/service/AddressBookService.java
@@ -1,6 +1,9 @@
 package edu.czjt.reggie.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.czjt.reggie.entity.AddressBook;
 
-public interface AddressBookService  {
+public interface AddressBookService extends IService<AddressBook> {
+    //Service接口，提供了常用的CRUD（创建、读取、更新、删除）方法
 
 }
