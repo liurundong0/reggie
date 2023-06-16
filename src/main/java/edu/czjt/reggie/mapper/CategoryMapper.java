a812d544e1e9f2fa43c98c233528df0ba4bdb0a8
xujiakai0715 <136038543+xujiakai0715@users.noreply.github.com>
2023-06-16 09:03:41 +0800

    Update CategoryMapper.java

--- src/main/java/edu/czjt/reggie/mapper/CategoryMapper.java
+++ src/main/java/edu/czjt/reggie/mapper/CategoryMapper.java
@@ -3,9 +3,12 @@ package edu.czjt.reggie.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import edu.czjt.reggie.entity.Category;
 import org.apache.ibatis.annotations.Mapper;
+
+
 /**
  * Created by xujiakai on 2023/6/14
   */
+
 @Mapper
 public interface CategoryMapper extends BaseMapper<Category> {
 }
