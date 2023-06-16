747030d418c45f352be3a89a93ef387ce93e21a2
xujiakai0715 <136038543+xujiakai0715@users.noreply.github.com>
2023-06-16 08:41:57 +0800

    Update CategoryServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/CategoryServiceImpl.java
@@ -1,5 +1,14 @@
 package edu.czjt.reggie.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import edu.czjt.reggie.entity.Category;
+import edu.czjt.reggie.mapper.CategoryMapper;
+import edu.czjt.reggie.service.CategoryService;
+import org.springframework.stereotype.Service;
 
-public class CategoryServiceImpl  {
+/**
+ * Created by xujiakai on 2023/6/14
+  */
+@Service
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 }
