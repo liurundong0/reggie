ab1f6c58f791666cfb74e94e5937a4b986c12c1e
xujiakai0715 <136038543+xujiakai0715@users.noreply.github.com>
2023-06-16 09:04:09 +0800

    Update CategoryService.java

--- src/main/java/edu/czjt/reggie/service/CategoryService.java
+++ src/main/java/edu/czjt/reggie/service/CategoryService.java
@@ -3,9 +3,11 @@ package edu.czjt.reggie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.entity.Category;
 
+
 /**
 * Created by xujiakai on 2023/6/14
   */
+
 public interface CategoryService extends IService<Category> {
 }
   
