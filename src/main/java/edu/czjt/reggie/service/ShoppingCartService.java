f82f263737eec20f2f9b869349974f243ebdabcd
liurundong0 <72503448+liurundong0@users.noreply.github.com>
2023-06-16 09:12:24 +0800

    Update ShoppingCartService.java

--- src/main/java/edu/czjt/reggie/service/ShoppingCartService.java
+++ src/main/java/edu/czjt/reggie/service/ShoppingCartService.java
@@ -3,6 +3,6 @@ package edu.czjt.reggie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.entity.ShoppingCart;
 
-public interface ShoppingCartService  {
+public interface ShoppingCartService extends IService<ShoppingCart> {
 
 }
